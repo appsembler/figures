0472db78e90b636f4dbadb9fe2cfeef64a355701
Matej Grozdanović <grozdanowski@gmail.com>
2018-01-25 17:49:30 +0100

    wrap up the course select autocompleter and optimise to work with damn edx styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,13 @@ import React, { Component } from 'react';
 import { Route, NavLink } from 'react-router-dom';
 import Dashboard from 'base/views/Dashboard';
 import Test from 'base/views/Test';
+import 'base/sass/base/_base-overrides.scss';
 import styles from 'base/sass/base/_grid.scss';
 
 class App extends Component {
   render() {
     return (
       <main className={styles['layout-root']}>
-        <header>
-          <NavLink to="/figures/dashboard">Dashboard</NavLink>
-          <NavLink to="/figures/test">Test</NavLink>
-        </header>
         <Route exact path="/figures/dashboard" component={Dashboard} />
         <Route exact path="/figures/test" component={Test} />
       </main>
