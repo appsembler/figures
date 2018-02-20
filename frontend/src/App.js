602b1c309dbe4f2a6e4ddaadbf5d3f73560a6cbd
Matej Grozdanović <grozdanowski@gmail.com>
2018-02-20 17:26:29 +0100

    add fancy transitions between views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
-import Dashboard from 'base/views/Dashboard';
-import Test from 'base/views/Test';
+import { Route, Switch } from 'react-router-dom';
+import ReactCSSTransitionReplace from 'react-css-transition-replace';
+import HeaderAreaLayout from 'base/components/layout/HeaderAreaLayout';
+import HeaderContentMaus from 'base/components/header-views/HeaderContentMaus';
+import HeaderContentReportsList from 'base/components/header-views/HeaderContentReportsList';
+import { history } from './store';
 import 'base/sass/base/_base-overrides.scss';
 import styles from 'base/sass/base/_grid.scss';
 
@@ -9,8 +12,27 @@ class App extends Component {
   render() {
     return (
       <main className={styles['layout-root']}>
-        <Route exact path="/figures/dashboard" component={Dashboard} />
-        <Route exact path="/figures/test" component={Test} />
+        <div className="ef--layout-root">
+          <HeaderAreaLayout location={history.location.pathname}>
+            <Route render={ ({location}) => (
+              <ReactCSSTransitionReplace
+                transitionName='page'
+                transitionEnterTimeout={400}
+                transitionLeaveTimeout={400}
+              >
+                <div key={history.location.pathname}>
+                  <Switch location={history.location}>
+                    <Route exact path="/figures/dashboard" component={HeaderContentMaus} />
+                    <Route exact path="/figures/test" component={HeaderContentReportsList} />
+                  </Switch>
+                </div>
+              </ReactCSSTransitionReplace>
+            )}/>
+          </HeaderAreaLayout>
+          <section className="ef--container ef--base-grid-layout">
+
+          </section>
+        </div>
       </main>
     );
   }
