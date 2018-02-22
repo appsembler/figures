64c7020bcda7444072dde5f990f7e3caae959674
Matej Grozdanović <grozdanowski@gmail.com>
2018-02-22 15:48:21 +0100

    reorganise single component code so it’s easier to add new components later

---
+++ frontend/src/components/header-views/header-content-course/HeaderContentCourse.js
@@ -0,0 +1,201 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import styles from './_header-content-course.scss';
+import { ResponsiveContainer, BarChart, Bar, XAxis, Tooltip, LabelList } from 'recharts';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faAngleDoubleUp, faAngleDoubleDown } from '@fortawesome/fontawesome-free-solid';
+
+let cx = classNames.bind(styles);
+
+class CustomTooltip extends Component {
+
+  render() {
+    const { active } = this.props;
+
+    if (active) {
+      const { payload, label } = this.props;
+      return (
+        <div className={styles['bar-tooltip']}>
+          <span className={styles['tooltip-value']}>{payload[0].value}</span>
+          <p>learners currently at this section</p>
+        </div>
+      );
+    }
+
+    return null;
+  }
+}
+
+class HeaderContentCourse extends Component {
+
+  render() {
+    let currentPeriodValue = this.props.data[this.props.data.length-1].value;
+    let previousPeriodValue = this.props.data[this.props.data.length-2].value;
+    let comparisonIcon;
+    let comparisonValue;
+    if (currentPeriodValue >= previousPeriodValue) {
+      comparisonIcon = <FontAwesomeIcon icon={faAngleDoubleUp} />;
+      comparisonValue = currentPeriodValue - previousPeriodValue;
+    } else {
+      comparisonIcon = <FontAwesomeIcon icon={faAngleDoubleDown} />;
+      comparisonValue = previousPeriodValue - currentPeriodValue;
+    }
+
+    return (
+      <section className={styles['header-content-course']}>
+        <div className={cx({ 'main-content': true, 'container': true})}>
+          <div className={styles['course-title']}>
+            {this.props.courseTitle}
+          </div>
+          <div className={styles['course-info']}>
+            <span className={styles['course-code']}>{this.props.courseCode}</span>
+            <span className={styles['course-info-separator']}>|</span>
+            {this.props.courseIsSelfPaced ? (
+              <span className={styles['course-date']}>This course is self-paced</span>
+            ) : [
+              <span className={styles['course-date']}>Starts: {this.props.courseStartDate}</span>,
+              <span className={styles['course-info-separator']}>|</span>,
+              <span className={styles['course-date']}>Ends: {this.props.courseEndDate}</span>,
+            ]}
+          </div>
+          <span className={styles['text-separator']} />
+          <div className={styles['learners-info']}>
+            <strong>{this.props.courseLearnersCount}</strong> learners currently enrolled, progressing through sections as displayed below:
+          </div>
+        </div>
+        <div className={cx({ 'graph-bars-container': true, 'container': true})}>
+          <ResponsiveContainer width="100%" height={140}>
+            <BarChart
+              data={this.props.data}
+              margin={{top: 0, bottom: 0, left: 0, right: 0}}
+              barCategoryGap={4}
+            >
+              <Tooltip
+                content={<CustomTooltip/>}
+                cursor={{ fill: 'rgba(255, 255, 255, 0.15)'}}
+                offset={0}
+              />
+              <Bar dataKey='value' stroke='none' fill='#ffffff' fillOpacity={0.8} />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+        <div className={styles['graph-labels-wrapper']}>
+          <div className={cx({ 'graph-labels-container': true, 'container': true})}>
+            <ResponsiveContainer width="100%" height={155}>
+              <BarChart
+                data={this.props.data}
+                margin={{top: 0, bottom: 0, left: 0, right: 0}}
+                barCategoryGap={4}
+              >
+                <XAxis
+                  dataKey='lessonTitle'
+                  axisLine={false}
+                  tickLine={false}
+                  height={155}
+                  angle={90}
+                  textAnchor="start"
+                  interval={0}
+                  //tick={<CustomXAxisLabel />}
+                />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+HeaderContentCourse.defaultProps = {
+  courseTitle: 'This is a course name, and oh my it is just sooo long',
+  courseCode: 'A193 - 2016Q4',
+  courseIsSelfPaced: false,
+  courseStartDate: 'October 27th, 2017',
+  courseEndDate: 'December 15th, 2018',
+  courseLearnersCount: 561,
+  data: [
+    {
+      lessonTitle: 'Lesson 1',
+      lessonSubtitle: 'Something something something',
+      value: 12
+    },
+    {
+      lessonTitle: 'Lesson 2',
+      lessonSubtitle: 'Something something something something',
+      value: 19
+    },
+    {
+      lessonTitle: 'Lesson 3',
+      lessonSubtitle: 'Something something',
+      value: 38
+    },
+    {
+      lessonTitle: 'Lesson 4',
+      lessonSubtitle: 'Something something something',
+      value: 31
+    },
+    {
+      lessonTitle: 'Lesson 5',
+      lessonSubtitle: 'Something something something',
+      value: 59
+    },
+    {
+      lessonTitle: 'Lesson 6',
+      lessonSubtitle: 'Something something something',
+      value: 12
+    },
+    {
+      lessonTitle: 'Lesson 7',
+      lessonSubtitle: 'Something',
+      value: 14
+    },
+    {
+      lessonTitle: 'Lesson 8',
+      lessonSubtitle: 'Something something something',
+      value: 7
+    },
+    {
+      lessonTitle: 'Lesson 9',
+      lessonSubtitle: 'Something something something',
+      value: 9
+    },
+    {
+      lessonTitle: 'Lesson 10',
+      lessonSubtitle: 'Something something something',
+      value: 16
+    },
+    {
+      lessonTitle: 'Lesson 11',
+      lessonSubtitle: 'Something something something',
+      value: 22
+    },
+    {
+      lessonTitle: 'Lesson 12',
+      lessonSubtitle: 'Something something something',
+      value: 12
+    },
+    {
+      lessonTitle: 'Lesson 13',
+      lessonSubtitle: 'Something something something',
+      value: 3
+    },
+    {
+      lessonTitle: 'Lesson 14',
+      lessonSubtitle: 'Something something something',
+      value: 14
+    },
+    {
+      lessonTitle: 'Lesson 15',
+      lessonSubtitle: 'Something something something',
+      value: 30
+    },
+    {
+      lessonTitle: 'Lesson 16',
+      lessonSubtitle: 'Something something something',
+      value: 14
+    }
+  ]
+}
+
+export default HeaderContentCourse;
