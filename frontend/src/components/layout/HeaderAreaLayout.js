0772bdc04981e22a882fa1d40e0ac8415e5d0e28
Matej Grozdanović <grozdanowski@gmail.com>
2018-05-04 16:29:36 +0200

    make header logo clickable to lead to Dashboard

--- frontend/src/components/layout/HeaderAreaLayout.js
+++ frontend/src/components/layout/HeaderAreaLayout.js
@@ -3,6 +3,7 @@ import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 import styles from './_header-area-layout.scss';
 import HeaderNav from 'base/components/layout/HeaderNav';
+import { NavLink } from 'react-router-dom';
 import figuresLogo from 'base/images/logo/edx-figures--logo--negative.svg';
 
 let cx = classNames.bind(styles);
@@ -14,9 +15,12 @@ class HeaderAreaLayout extends Component {
     return (
       <div className={styles['header-area']}>
         <div className={cx({ 'header-top': true, 'container': true })}>
-          <div className={styles['header-logo-container']}>
+          <NavLink
+            to="/figures"
+            className={styles['header-logo-container']}
+          >
             <img src={figuresLogo} alt="EdX Figures" role="presentation" />
-          </div>
+          </NavLink>
           <HeaderNav />
         </div>
         {this.props.children}
