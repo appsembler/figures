be798e88b3f7c0f86c9711c0d958a7a4ff6aed1c
Matej Grozdanović <grozdanowski@gmail.com>
2018-07-31 02:04:22 +0200

    update api config

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -6,7 +6,7 @@ const apiConfig = {
   courseEnrollmentsApi: '/figures/api/course-enrollments/',
   generalSiteMetrics: '/figures/api/general-site-metrics/',
   coursesDetailed: '/figures/api/courses/detail/',
-  learnersDetailed: '/figures/api/learners/detail/',
+  learnersDetailed: '/figures/api/users/detail/',
 }
 
 export default apiConfig;
