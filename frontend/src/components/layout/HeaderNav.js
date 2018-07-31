abcf1ba0be886ae5f8544aa1c28001c9efe8ca8c
Matej Grozdanović <grozdanowski@gmail.com>
2018-07-31 02:00:41 +0200

    improve redux setup, move data init from header component to app root, add loader spinner and api fetching indication

--- frontend/src/components/layout/HeaderNav.js
+++ frontend/src/components/layout/HeaderNav.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchCoursesIndex, fetchUserIndex, fetchGeneralData } from 'base/redux/actions/Actions';
 import { NavLink } from 'react-router-dom';
 import styles from './_header-nav.scss';
 import AutoCompleteCourseSelect from 'base/components/inputs/AutoCompleteCourseSelect';
 
 class HeaderNav extends Component {
-
-  componentDidMount() {
-    this.props.fetchCoursesIndex();
-    this.props.fetchUserIndex();
-    this.props.fetchGeneralData();
-  }
 
   render() {
     return (
@@ -37,17 +29,4 @@ class HeaderNav extends Component {
   }
 }
 
-const mapStateToProps = (state, ownProps) => ({
-
-})
-
-const mapDispatchToProps = dispatch => ({
-  fetchCoursesIndex: () => dispatch(fetchCoursesIndex()),
-  fetchUserIndex: () => dispatch(fetchUserIndex()),
-  fetchGeneralData: () => dispatch(fetchGeneralData()),
-})
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(HeaderNav)
+export default HeaderNav
