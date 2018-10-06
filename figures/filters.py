38773c3a95c97b6d244cdd4c082c640c222bf842
John Baldwin <jlbaldwin@gmail.com>
2018-10-06 04:46:59 -0400

    filters module now at 100% coverage and test_filters.py now passes PEP8

--- figures/filters.py
+++ figures/filters.py
@@ -157,38 +157,3 @@ class SiteDailyMetricsFilter(django_filters.FilterSet):
     class Meta:
         model = SiteDailyMetrics
         fields = ['date_for', 'date']
-
-
-class LearnerFilterSet(django_filters.FilterSet):
-    '''Provides filtering for Learner based views
-
-    '''
-    is_active = django_filters.BooleanFilter(name='is_active',)
-    course_id = django_filters.MethodFilter(action='filter_course_id')
-    user_ids = django_filters.MethodFilter(action='filter_user_ids')
-
-    class Meta:
-        model = get_user_model()
-        fields = ['username', 'course_id', 'user_ids', 'is_active']
-
-    def filter_course_id(self, queryset, course_id_str):
-        '''
-
-        This method converts the course id string to a CourseLocator object
-        and returns the filtered queryset. This is required because
-        CourseEnrollment course_id fields are of type CourseKeyField
-
-        Query parameters with plus signs '+' in the string are automatically
-        replaced with spaces, so we need to put the '+' back in for CourseKey
-        to be able to create a course key object from the string
-        '''
-        course_key = CourseKey.from_string(course_id_str.replace(' ', '+'))
-        # get course enrollments for the course
-        user_ids = CourseEnrollment.objects.filter(
-            course_id=course_key).values_list('user__id', flat=True).distinct()
-        return queryset.filter(id__in=user_ids)
-
-    def filter_user_ids(self, queryset, user_ids_str):
-
-        user_ids = [id for id in user_ids_str.split(',') if id.isdigit()]
-        return queryset.filter(id__in=user_ids)
