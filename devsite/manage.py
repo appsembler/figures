6c72f25d3df9e0e3796a8f156bfb1af54af56bdd
John Baldwin <jlbaldwin@gmail.com>
2019-03-23 16:14:54 -0400

    WIP seed data

--- devsite/manage.py
+++ devsite/manage.py
@@ -3,10 +3,21 @@ import os
 import sys
 
 if __name__ == "__main__":
-    # insert the parent dir in sys.path to find our reusable app
-    sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    # Insert the path of the platform mocks to locate those
+    # Note: The mocks will be moved to the devsite for release 0.3.0 or 0.4.0
+    sys.path.insert(0, os.path.join(project_root, 'tests/mocks'))
+
+    # Insert the project root dir to find our reusable app
+    sys.path.insert(0, project_root)
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devsite.settings")
 
     from django.core.management import execute_from_command_line
+
+    # for path in sys.path:
+    #     print(path)
 
     execute_from_command_line(sys.argv)
