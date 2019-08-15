1255f1859f7a2aa984b39a99308cb7111fa8544f
Matej Grozdanović <grozdanowski@gmail.com>
2019-08-15 14:27:55 +0200

    Fix a lot of vulnerabilities in JS packages (#110)
    * fix vulnerabilities, update babel-jest, css-loader
    
    * update postcss-loader
    
    * update webpack-bundle-tracker
    
    * do a final rebuild on defaults
    
    * quickly fix a super annoying bug where links are the wrong color
    
    * final rebuild for this work

--- frontend/config/webpack.config.prod.js
+++ frontend/config/webpack.config.prod.js
@@ -181,8 +181,9 @@ module.exports = {
                 {
                   loader: 'css-loader',
                   options: {
-                    modules: true,
-                    localIdentName: '[local]__[hash:base64:8]'
+                    modules: {
+                      localIdentName: '[local]__[hash:base64:8]'
+                    }
                   }
                 },
                 'postcss-loader'
@@ -215,10 +216,11 @@ module.exports = {
             {
               loader: 'css-loader',
               options: {
-                modules: true,
+                modules: {
+                  localIdentName: '[local]__[hash:base64:8]'
+                },
                 sourceMap: true,
                 importLoaders: 2,
-                localIdentName: '[local]__[hash:base64:8]'
               }
             },
             {
