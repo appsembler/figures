b9129f7c12fbe183d90167f5be176b79e967ed57
John Baldwin <jlbaldwin@gmail.com>
2019-10-28 21:14:29 +0100

    Devsite: Added more courses to test front end courses list
    * Updated Faker to most recent release
    * Minor updates to the deveveloper quickstart guide

--- devsite/devsite/cans/course_overviews.py
+++ devsite/devsite/cans/course_overviews.py
@@ -1,4 +1,6 @@
 """
+
+This module provides canned courses to test specific criteria
 TODO:
 * Add self-paced flag
 """
@@ -14,6 +16,7 @@ COURSE_OVERVIEW_DATA = [
         enrollment_start='2018-08-01',
         enrollment_end='2018-12-31',
         ),
+    # The following provide two identical courses with different runs
     dict(
         id='course-v1:StarFleetAcademy+SFA02+2161',
         display_name='Intro to Xenology',
@@ -23,5 +26,15 @@ COURSE_OVERVIEW_DATA = [
         created='2018-09-01',
         enrollment_start='2018-10-05',
         enrollment_end='2019-02-02',
+        ),
+    dict(
+        id='course-v1:StarFleetAcademy+SFA02+2162',
+        display_name='Intro to Xenology',
+        org='StarFleetAcademy',
+        display_org_with_default='StarFleetAcademy',
+        number='SFA03',
+        created='2019-09-01',
+        enrollment_start='2019-10-05',
+        enrollment_end='2020-02-02',
         ),
 ]
