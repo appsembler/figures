acaedfb28bee3c0fea1d87d002ad6f587b567720
John Baldwin <jlbaldwin@gmail.com>
2019-10-30 09:50:17 +0100

    Added unit test for 'FiguresKiloPagination

--- tests/test_pagination.py
+++ tests/test_pagination.py
@@ -1,9 +1,14 @@
-from figures.pagination import FiguresLimitOffsetPagination
+from figures.pagination import (
+    FiguresLimitOffsetPagination,
+    FiguresKiloPagination,
+)
 
 
 class TestFiguresLimitOffsetPagination(object):
-    '''
-
-    '''
     def test_default_pagination_limit(self):
         assert FiguresLimitOffsetPagination.default_limit == 20
+
+
+class TestFigureKiloPagination(object):
+    def test_default_pagination_limit(self):
+        assert FiguresKiloPagination.default_limit == 1000
