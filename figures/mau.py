6a6e669c7af5db5d7d9e5dc8aa93cad7d475377c
Omar Al-Ithawi <i@omardo.com>
2020-02-11 18:31:06 +0200

    Fixes the pylint warnings below:
     - bad-whitespace
     - pointless-string-statement
     - simplifiable-if-statement
     - unused-import
     - unused-variable
     - useless-suppression

--- figures/mau.py
+++ figures/mau.py
@@ -74,7 +74,7 @@ def store_mau_metrics(site, overwrite=False):
                                             month=today.month)
 
     # store site data
-    site_mau_obj, created = SiteMauMetrics.save_metrics(site=site,
+    site_mau_obj, _created = SiteMauMetrics.save_metrics(site=site,
                                                         date_for=today.date(),
                                                         data=dict(mau=site_mau.count()),
                                                         overwrite=overwrite)
@@ -85,7 +85,7 @@ def store_mau_metrics(site, overwrite=False):
             student_modules=course_student_modules,
             year=today.year,
             month=today.month)
-        course_mau_obj, created = CourseMauMetrics.save_metrics(
+        course_mau_obj, _created = CourseMauMetrics.save_metrics(
                 site=site,
                 course_id=str(course_key),
                 date_for=today.date(),
