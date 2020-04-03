f7b73a129cb989dc10627654577d0878af29adb0
Matej Grozdanović <grozdanowski@gmail.com>
2020-04-03 17:01:24 +0200

    cosmetic reordering

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,10 +1,10 @@
 const apiConfig = {
-  coursesGeneral: '/figures/api/courses/general/',
   figuresUsersIndexApi: '/figures/api/user-index/',
   edxCourseInfoApi: '/api/courses/v1/courses/',
   edxUserInfoApi: '/api/user/v1/accounts/',
   courseEnrollmentsApi: '/figures/api/course-enrollments/',
   generalSiteMetrics: '/figures/api/general-site-metrics/',
+  coursesGeneral: '/figures/api/courses/general/',
   coursesDetailed: '/figures/api/courses/detail/',
   learnersGeneral: '/figures/api/users/general/',
   learnersDetailed: '/figures/api/users/detail/',
