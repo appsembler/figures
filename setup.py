b6456b1daef2cfd078598bcaf44c2d720b7d4790
John Baldwin <jlbaldwin@gmail.com>
2020-12-15 19:58:57 +0100

    Bump version to 0.4.dev5

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='Figures',
-    version='0.4.dev4',
+    version='0.4.dev5',
     packages=find_packages(),
     include_package_data=True,
     license='MIT',
