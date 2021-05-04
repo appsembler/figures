f730491259738893365b766277e4c293850a5c21
Esther <estherjsuh@protonmail.com>
2021-05-04 09:57:34 -0700

    add test for populate_daily_metrics

--- figures/tasks.py
+++ figures/tasks.py
@@ -10,7 +10,7 @@ import time
 
 import six
 import waffle
-import os
+
 from django.contrib.sites.models import Site
 from django.utils.timezone import utc
 
@@ -53,10 +53,10 @@ def populate_single_cdm(course_id, date_for=None, force_update=False):
     The calling function is responsible for error handling calls to this
     function
     """
-    if waffle.switch_is_active(WAFFLE_DISABLE_PIPELINE):
-        logger.info('%s: Disabled due to %s being active.', populate_single_cdm.__name__,
-            WAFFLE_DISABLE_PIPELINE)
-        return
+    # if waffle.switch_is_active(WAFFLE_DISABLE_PIPELINE):
+    #     logger.info('%s: Disabled due to %s being active.', populate_single_cdm.__name__,
+    #         WAFFLE_DISABLE_PIPELINE)
+    #     return
 
     if date_for:
         date_for = as_date(date_for)
@@ -175,12 +175,20 @@ def populate_daily_metrics(date_for=None, force_update=False):
 
     This function will get reworked so that each site runs in its own
     """
+
+    #Waffle Switch
+    if waffle.switch_is_active(WAFFLE_DISABLE_PIPELINE):
+        logger.info('%s: Disabled due to %s being active.', populate_single_cdm.__name__, 
+    WAFFLE_DISABLE_PIPELINE)
+        return
+
     # The date_for handling is very similar to the new rule we ahve in
     # `figures.pipeline.helpers.pipeline_data_for_rule`
     # The difference is the following code does not set 'date_for' as yesterday
     # So we likely want to rework the pipeline rule function and this code
     # so that we have a generalized date_for rule that can take an optional
     # transform function, like `prev_day`
+
     today = datetime.datetime.utcnow().replace(tzinfo=utc).date()
     # TODO: Decide if/how we want any special logging if we get an exception
     # on 'casting' the date_for argument as a datetime.date object
@@ -386,6 +394,12 @@ def run_figures_monthly_metrics():
     """
     Populate monthly metrics for all sites.
     """
+    #Waffle Switch
+    if waffle.switch_is_active(WAFFLE_DISABLE_PIPELINE):
+        logger.info('%s: Disabled due to %s being active.', populate_single_cdm.__name__,
+            WAFFLE_DISABLE_PIPELINE)
+        return
+
     logger.info('Starting figures.tasks.run_figures_monthly_metrics...')
     for site in get_sites():
         populate_monthly_metrics_for_site.delay(site_id=site.id)
