c72cee8d8ec556547a32e0c9b1f0b314cde975b1
John Baldwin <jlbaldwin@gmail.com>
2021-06-02 17:06:53 +0200

    Reset expected months back for SMM ViewSet
    SMM - SiteMonthlyMetrics. This one is a bit weird and I need to dig into
    it, I think. Tests started to fail because only 6 months back was
    returned for the SiteMontlyMetricsViewSet list returned. Now it is
    returning 7 months again, as it did before

--- tests/views/test_site_monthly_metrics_viewset.py
+++ tests/views/test_site_monthly_metrics_viewset.py
@@ -92,7 +92,7 @@ class TestSiteMonthlyMetricsViewSet(BaseViewTest):
         if organizations_support_sites():
             settings.FEATURES['FIGURES_IS_MULTISITE'] = True
         super(TestSiteMonthlyMetricsViewSet, self).setup(db)
-        self.months_back = 6
+        self.months_back = 7
 
     def check_response(self, response, endpoint):
         assert response.status_code == status.HTTP_200_OK
