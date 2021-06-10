ff635ccea04fc83f680464473ccc7db47e447881
Bryan Wilson <bryanlandia@gmail.com>
2021-06-09 18:34:43 -0700

    Fix update_enrollment_data task call in backfill_enrollment_data

--- figures/management/commands/backfill_enrollment_data.py
+++ figures/management/commands/backfill_enrollment_data.py
@@ -24,8 +24,8 @@ class Command(BaseBackfillCommand):
         for site_id in self.get_site_ids(options['site']):
             print('Updating EnrollmentData for site {}'.format(site_id))
             if options['no_delay']:
-                update_enrollment_data(site_ids=(site_id))
+                update_enrollment_data(site_id=site_id)
             else:
-                update_enrollment_data.delay(site_ids=(site_id))  # pragma: no cover
+                update_enrollment_data.delay(site_id=site_id)  # pragma: no cover
 
         print('DONE: Backfill Figures EnrollmentData')
