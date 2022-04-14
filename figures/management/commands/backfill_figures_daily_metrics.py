97d53461f68c400c3724457c4199732794d89072
John Baldwin <jlbaldwin@gmail.com>
2022-04-13 20:03:44 -0400

    Adds daily metrics backfill functions
    This commit has new functions in `figures.pipeline.backfill`.
    For each function, see the details in each function's docstring
    
    Because we don't want to update courses created after the "date_for", we
    need to identify which courses to update. That's what the bulk of this
    commit's code does
    
    It uses the existing metrics loader classes to do the actual data
    querying and metrics model saves
    
    Add backfill dialy metrics managmenet command
    
    * Still working on getting the mgmt test to pass. Having errors in parsing
    the args call to `call_command`, Will pick up again to get this working
    tomorrow

--- figures/management/commands/backfill_figures_daily_metrics.py
+++ figures/management/commands/backfill_figures_daily_metrics.py
@@ -11,14 +11,14 @@ from textwrap import dedent
 
 from dateutil.rrule import rrule, DAILY
 
-from figures.management.base import BaseBackfillCommand
-from figures.tasks import (
-    populate_daily_metrics,
-    experimental_populate_daily_metrics
-)
+from django.core.management.base import BaseCommand, CommandError
+
+from figures.helpers import as_date
+from figures.sites import Site
+from figures.pipeline.backfill import backfill_daily_metrics_for_site_and_date
 
 
-class Command(BaseBackfillCommand):
+class Command(BaseCommand):
     '''Populate Figures daily metrics models (``CourseDailyMetrics`` and ``SiteDailyMetrics``).
     Note that correctly populating cumulative user and course count for ``SiteDailyMetrics``
     relies on running this sequentially forward from the first date for which StudentModule records
@@ -27,57 +27,83 @@ class Command(BaseBackfillCommand):
 
     help = dedent(__doc__).strip()
 
+    def do_backfill(self, site, date_for, **kwargs):
+        """Thin wrapper around backfill function to handle parameters
+        """
+        if kwargs.get('skip_sdm', False):
+            process_sdm = False
+        else:
+            process_sdm = True
+        logdir = kwargs.get('logdir', None)
+        force_update = kwargs.get('force_update', False)
+
+        return 'nop'
+        return backfill_daily_metrics_for_site_and_date(site,
+                                                        date_for,
+                                                        process_sdm=process_sdm,
+                                                        logdir=logdir,
+                                                        force_update=force_update)
+
+    def get_site(self, options):
+        """Return a Site object matching the command line arg
+
+        The 'site' option can be a domain name (sans protocol) or the site
+        record primary key
+        """
+        identifier = options['site']
+        try:
+            filter_arg = dict(pk=int(identifier))
+        except ValueError:
+            filter_arg = dict(domain=identifier)
+        return Site.objects.get(**filter_arg)
+
+    def get_dates(self, options):
+        """Return a list of dates
+        """
+        if options['date'] and options['date_range']:
+            raise CommandError(
+                'Select either "--date" or "--date-range" option, but not both')
+        if not options['date'] and not options['date_range']:
+            raise CommandError(
+                'You need to select one of "--date" or "--date-range" parameters')
+        if options['date']:
+            return [as_date(options['date'])]
+        else:
+            dates = sorted([as_date(rec) for rec in options['date_range']])
+            # The '--date-range' CLI argument enforces two values
+            return [dt.date() for dt in rrule(freq=DAILY,
+                                              dtstart=dates[0],
+                                              until=dates[1])]
+
     def add_arguments(self, parser):
-        parser.add_argument(
-            '--experimental',
-            action='store_true',
-            default=False,
-            help=('Run with Celery workflows (Warning: This is still under'
-                  ' development and likely to get stuck/hung jobs')
-        )
-        super(Command, self).add_arguments(parser)
+        parser.add_argument('site', help='Site domain or id')
+
+        parser.add_argument('--date', help='Run backfill for a single date')
+        parser.add_argument('--date-range', nargs=2,
+                            help='Run backfill from a start date to an end date')
+
+        # by default we process the SDM. If we do not want to, set this flag
+        parser.add_argument('--skip-sdm', action='store_true',
+                            help='Set this flag to avoid collecting SDM records')
+        parser.add_argument('--force-update', action='store_true',
+                            help='Update records if they already exist')
+        parser.add_argument('--logdir', default=None,
+                            help='altnerate path to output log files')
 
     def handle(self, *args, **options):
-        '''
-        Note the '# pragma: no cover' lines below. This is because we are not
-        yet mocking celery for test coverage
-        '''
-        date_start = self.get_date(options['date_start'])
-        date_end = self.get_date(options['date_end'])
+        site = self.get_site(options)
+        dates = self.get_dates(options)
+        backfill_options = ['skip_sdm', 'force_update', 'logdir']
+        extra_args = dict((key, options[key]) for key in backfill_options)
+        # import pdb; pdb.set_trace()
 
-        experimental = options['experimental']
-
-        print('BEGIN RANGE: Backfilling Figures daily metrics for dates {} to {}'.format(
-            date_start, date_end
-        ))
-
-        # don't pass multiple site ids to tasks
-        site_id = None if not options['site'] else self.get_site_ids(options['site'])
-
-        # populate daily metrics one day at a time for date range
-        for dt in rrule(DAILY, dtstart=date_start, until=date_end):
-
-            print('BEGIN: Backfill Figures daily metrics metrics for: {}'.format(dt))
-
-            kwargs = dict(
-                site_id=site_id,
-                date_for=str(dt),
-                force_update=options['overwrite']
-            )
-
-            if experimental:
-                metrics_func = experimental_populate_daily_metrics
-                del kwargs['site_id']  # not implemented for experimental
-            else:
-                metrics_func = populate_daily_metrics
-
-            if options['no_delay']:
-                metrics_func(**kwargs)
-            else:
-                metrics_func.delay(**kwargs)  # pragma: no cover
-
-            print('END: Backfill Figures daily metrics metrics for: {}'.format(dt))
-
-        print('END RANGE: Backfilling Figures daily metrics for dates {} to {}'.format(
-            date_start, date_end
-        ))
+        for date_for in dates:
+            print('Generating daily metrics for date: {}'.format(date_for.isoformat()))
+            results = self.do_backfill(site=site,
+                                       date_for=date_for,
+                                       **extra_args)
+            print('Finished date {}. Processed {} courses'.format(
+                date_for.isoformat(), results['courses_processed']))
+            print('Wrote log file: "{}"'.format(results['logfile']))
+            print('CDM processing time: {}, SDM processing time: {}'.format(
+                results['cdm_elapsed'], results['sdm_elapsed']))
--- figures/pipeline/backfill.py
+++ figures/pipeline/backfill.py
@@ -4,19 +4,53 @@ Initially developed to support API performance improvements
 """
 
 from __future__ import absolute_import
+import os
+from time import time
 from datetime import datetime
 from dateutil.rrule import rrule, MONTHLY
 from dateutil.relativedelta import relativedelta
 
+from django.conf import settings
 from django.utils.timezone import utc
 
 from figures.compat import CourseNotFound
+from figures.course import Course
+from figures.helpers import as_date
+from figures.models import EnrollmentData
 from figures.sites import (
     get_course_enrollments_for_site,
+    get_course_keys_for_site,
     get_student_modules_for_site
 )
+from figures.pipeline.course_daily_metrics import CourseDailyMetricsLoader
+from figures.pipeline.site_daily_metrics import SiteDailyMetricsLoader
 from figures.pipeline.site_monthly_metrics import fill_month
-from figures.models import EnrollmentData
+
+# Backfill is done irregularly on an as-needed basis and not part of regularly
+# scheduled operations.
+# Default to a directory for which the edxapp user already has permissions
+# This is an interim implementation is for expediency. We can then follow up
+# with discussion on where a more ideal location would be. Perhaps in
+# `/edx/var/log/figures`. However, the `/edx/var/log` directory does not have
+# write permission for the `edxapp` or `www-data` users, while `/edx/app/edxapp`
+# does. Therefore we initally declar the following to write backfill logs
+DEFAULT_FIGURES_BACKFILL_LOG_DIR = '/edx/app/edxapp/figures/logs/'
+
+
+class InvalidDataError(Exception):
+    """Raised when wrong data are used, like cross-site data processing
+
+    Initially created to raise an exception if cached data or data provided as
+    function parameters do not belong to the site being processed.
+    """
+    pass
+
+
+def figures_backfill_log_dir():
+    """Specify where to write Figures backfill log files
+    """
+    return settings.ENV_TOKENS['FIGURES'].get('BACKFILL_LOG_DIR',
+                                              DEFAULT_FIGURES_BACKFILL_LOG_DIR)
 
 
 # This function called just by mgmt command, backfill_figures_monthly_metrics.py
@@ -86,3 +120,136 @@ def backfill_enrollment_data_for_site(site):
                                      ce_id=rec.id))
 
     return dict(results=enrollment_data, errors=errors)
+
+
+def get_courses_first_enrollment_timestamps(site, as_strings=False):
+    """Return dict of course_id and first enrolled on date
+    key is the course_id, value is the date of the first enrollment
+
+    The dict returned by this function is used to determine which courses need
+    to be backfilled for a given date.
+
+    We have this as a seperate function so that the caller can generate the
+    mapping once to process over a set of days
+
+    The `as_strings` param is used to make it easier to serialize the data to
+    file
+    """
+    data = dict()
+    for course_key in get_course_keys_for_site(site):
+        created = Course(course_key).first_enrollment_timestamp()
+        if as_strings:
+            data[str(course_key)] = created.isoformat()
+        else:
+            data[course_key] = created
+    return data
+
+
+def courses_enrolled_on_or_before(site, date_for, data=None):
+    """list courses that have enrollments created on or befor the date
+    if data is passed in, assumes the date values are `datetime.date`
+
+    For the time being, this is a "getter" function instead of a generator so
+    that we can fail before updating Figures course daily metrics records in
+    the event that a set of data contain courses that do not belong to the site
+    """
+    check_course_ids = get_course_keys_for_site(site)
+    if data is None:
+        data = get_courses_first_enrollment_timestamps(site)
+    date_for = as_date(date_for)
+    course_ids = []
+    for course_id, created in data.items():
+        if course_id not in check_course_ids:
+            msg = 'Aborting: "Course {course_id}" does not belong to site "{domain}"'
+            raise InvalidDataError(msg.format(course_id=str(course_id),
+                                              domain=site.domain))
+
+        # if not created, then there are no enrollments in the course and we skip it
+        if created and as_date(created) <= date_for:
+            course_ids.append(course_id)
+    return course_ids
+
+
+def backfill_daily_metrics_for_site_and_date(site,
+                                             date_for,
+                                             process_sdm=True,
+                                             logdir=None,
+                                             force_update=False):
+    """To be run by Django management command or in Django shell
+
+    To note, this function was originally an ad-hoc script.
+
+    This function creates `CourseDailyMetrics` records for each course for the
+    specified site and day. It then creates the `SiteDailyMetrics` record for
+    the site and day.
+
+    Progress is tracked in a log file with the default directory in this
+    module's `DEFAULT_FIGURES_BACKFILL_LOG_DIR` variable. This can be overriden
+    in the settings "server-vars" file, like so:
+
+    ```
+    EDXAPP_ENV_EXTRA:
+      FIGURES:
+        BACKFILL_LOG_DIR: '/alternate/path/to/figures/backfill/logs'
+    ```
+
+    ## On progress
+
+    Progress will only be updated if we run backfill for the previous calendar
+    day. This is a bit of combined feature of Figures pipeline code and a
+    limitation in that there's not a ready way to extract historical progress
+    for a learner "on this date in the past".
+    """
+    date_for = as_date(date_for)
+    date_for_str = date_for.isoformat()
+
+    if logdir is None:
+        logdir = figures_backfill_log_dir()
+
+    filename = 'backfill-for-site-{site_id}-date-{date_for}.log'.format(
+        site_id=site.id, date_for=date_for_str)
+    filepath = os.path.join(logdir, filename)
+
+    course_ids = courses_enrolled_on_or_before(site, date_for)
+    course_id_count = len(course_ids)
+
+    with open(filepath, 'a', encoding='utf-8') as logfile:
+        start_time = time()
+        logfile.write('START: backfill {} courses, date_for: {}\n'.format(
+            course_id_count, date_for_str))
+        for i, course_id in enumerate(course_ids):
+            logfile.write('[{} of {}] date_for: {}, {}\n'.format(
+                i+1, course_id_count, date_for_str, str(course_id)))
+
+            cdm_obj, _created = CourseDailyMetricsLoader(
+                str(course_id)).load(date_for=date_for, force_update=force_update)
+            logfile.write('-- wrote CDM id: {}\n'.format(cdm_obj.id))
+
+            # We flush so we can tail the log file for progress
+            logfile.flush()
+        cdms_elapsed = time() - start_time
+        logfile.write('\nEND: backfill courses. date_for:{}, elapsed: {}\n'.format(
+            date_for_str, cdms_elapsed))
+        if process_sdm:
+            # TODO: if the SDM already exists, report it. This means the admin
+            # can decide to manually force update, which is usually what we
+            # want, however, we don't want surprise modifications of existing
+            # data, so we're leaving it up to the caller to explicitly say
+            # "destroy and rewrite"
+            logfile.write('START: backfill site {} for date {}: \n'.format(
+                site.domain, date_for_str))
+            start_time = time()
+            sdm_obj, _created = SiteDailyMetricsLoader().load(site=site,
+                                                              date_for=date_for,
+                                                              force_update=force_update)
+            logfile.write('-- wrote SDM id: {}\n'.format(sdm_obj.id))
+            sdm_elapsed = time() - start_time
+            logfile.write('\nEND: backfill site. date_for: {}, elapsed: {}\n'.format(
+                date_for_str, sdm_elapsed))
+
+    # return location of logfile and some instrumentation data
+    return dict(
+        logfile=filepath,
+        courses_processed=course_id_count,
+        cdms_elapsed=cdms_elapsed,
+        sdm_elapsed=sdm_elapsed)
